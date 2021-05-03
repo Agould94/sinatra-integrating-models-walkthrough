96da01419ba6658921dac3a28a47a58aaa4648a7
Alexander Gould <alexander.gould94@gmail.com>
2021-05-02 19:58:20 -0700

    commit tree d67ec4f591eb4fd9535a59f7660fc4202f4c05fd

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
+set :views, settings.root + '/4:26-4:30/sinatra-integrating-models-walkthrough/views'
   get '/' do
     erb :index
   end
 
   post '/' do
     text_from_user = params[:user_text]
-
+  
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+  
     erb :results
-  end
+    
+  end 
+  
 end
+#binding.pry
